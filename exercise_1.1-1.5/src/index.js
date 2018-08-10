208e03e7a8fea8cf4fab534044f75a558b26d68e
Tuukka Virtanen <duukkis@gmail.com>
2018-08-11 00:49:16 +0300

    Tehtävä 1.1 valmis

--- exercise_1.1-1.5/src/index.js
+++ exercise_1.1-1.5/src/index.js
@@ -2,22 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
+    const exercises1 = 10;
+    const exercises2 = 7;
+    const exercises3 = 14;
 
     return (
         <div>
-            <h1>{kurssi}</h1>
-            <p>{osa1} {tehtavia1}</p>
-            <p>{osa2} {tehtavia2}</p>
-            <p>{osa3} {tehtavia3}</p>
-            <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
+            <Otsikko name='Half Stack -sovelluskehitys' />
+            <Sisalto subject='Reactin perusteet' exercises={exercises1} />
+            <Sisalto subject='Tiedonvälitys propseilla' exercises={exercises2} />
+            <Sisalto subject='Komponenttien tila' exercises={exercises3} />
+            <Yhteensa exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
         </div>
+    )
+}
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.name}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <p>{props.subject} {props.exercises}</p>
+    )
+}
+
+const Yhteensa = (props) => {
+    return (
+        <p>yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää</p>
     )
 }
 
