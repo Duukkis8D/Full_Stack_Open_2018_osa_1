8b2b5aa5d1dd4d3b99799c7a4f555dfbb13e2292
Tuukka Virtanen <duukkis@gmail.com>
2018-08-11 00:11:08 +0300

    Esimerkkikoodi

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-    const now = new Date()
-    const a = 10
-    const b = 20
+const Hello = (props) => {
     return (
         <div>
-            <p>Hello world, it is {now.toString()}</p>
-            <p>{a} plus {b} is {a + b}</p>
+            <p>Hello {props.name}, you are {props.age} years old</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const nimi = 'Pekka'
+    const ika = 10
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name="Arto" age={26 + 10} />
+            <Hello name={nimi} age={ika} />
         </div>
     )
 }
