ced3a5f670778565fe7015f972ba78577acd2302
Tuukka Virtanen <duukkis@gmail.com>
2018-08-12 21:37:04 +0300

    Tehtävä 1.3 valmis

--- exercise_1.1-1.5/src/index.js
+++ exercise_1.1-1.5/src/index.js
@@ -2,40 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
+
     return (
         <div>
-            <Otsikko name='Half Stack -sovelluskehitys' />
-            <Sisalto/>
-            <Yhteensa exercises1={10} exercises2={7} exercises3={14} />
+            <Otsikko nimi={kurssi}/>
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3}/>
+            <Yhteensa tehtavia1={osa1.tehtavia} tehtavia2={osa2.tehtavia} tehtavia3={osa3.tehtavia}/>
         </div>
     )
 }
 
 const Otsikko = (props) => {
     return (
-        <h1>{props.name}</h1>
+        <h1>{props.nimi}</h1>
     )
 }
 
-const Sisalto = () => {
+const Sisalto = (props) => {
     return (
         <div>
-            <Osa subject='Reactin perusteet' exercises={10} />
-            <Osa subject='Tiedonvälitys propseilla' exercises={7} />
-            <Osa subject='Komponenttien tila' exercises={14} />
+            <Osa kurssi={props.osa1.nimi} tehtavia={props.osa1.tehtavia}/>
+            <Osa kurssi={props.osa2.nimi} tehtavia={props.osa2.tehtavia}/>
+            <Osa kurssi={props.osa3.nimi} tehtavia={props.osa3.tehtavia}/>
         </div>
     )
 }
 
 const Osa = (props) => {
     return (
-        <p>{props.subject} {props.exercises}</p>
+        <p>{props.kurssi} {props.tehtavia}</p>
     )
 }
 
 const Yhteensa = (props) => {
     return (
-        <p>yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää</p>
+        <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
     )
 }
 
