c7f1b993bb3eb1fc79a2cf5609d1cb5df981d941
Tuukka Virtanen <duukkis@gmail.com>
2018-08-20 19:55:04 +0300

    Tehtävä 1.6 valmis

--- exercise_1.6-1.11/src/index.js
+++ exercise_1.6-1.11/src/index.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-    return (
-        <AnnaPalautetta teksti={"anna palautetta"}/>
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            good: 0,
+            neutral: 0,
+            bad: 0
+        }
+    }
+
+    render() {
+        const handleGoodClick = () => () => this.setState({ good: this.state.good + 1 })
+
+        const handleNeutralClick = () => () => this.setState({ neutral: this.state.neutral + 1 })
+
+        const handleBadClick = () => () => this.setState({ bad: this.state.bad + 1 })
+
+        return (
+            <div>
+                <Otsikko teksti="anna palautetta"/>
+                <button onClick={handleGoodClick()}>hyvä</button>
+                <button onClick={handleNeutralClick()}>neutraali</button>
+                <button onClick={handleBadClick()}>huono</button>
+                <Otsikko teksti="statistiikka"/>
+                <Statistiikka state={this.state}/>
+            </div>
+        )
+    }
 }
 
-const AnnaPalautetta = (props) => {<h1>{props.teksti}</h1>}
+const Otsikko = (props) => <h1>{props.teksti}</h1>
+
+const Statistiikka = (props) => {
+    return (
+        <div>
+            {console.log("Statistiikka-komponentista", props.state)}
+            <p>hyvä {props.state.good}</p>
+            <p>neutraali {props.state.neutral}</p>
+            <p>huono {props.state.bad}</p>
+        </div>
+    )
+}
 
 ReactDOM.render(
     <App />,
